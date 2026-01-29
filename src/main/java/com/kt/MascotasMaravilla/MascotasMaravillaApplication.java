eede816a3b3d1c374e2135c07665b31286a509d1
Kevin <kfornier@gmail.com>
2026-01-29 20:57:10 -0300

    home and db

---
+++ src/main/java/com/kt/MascotasMaravilla/MascotasMaravillaApplication.java
@@ -0,0 +1,14 @@
+package com.kt.MascotasMaravilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class MascotasMaravillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotasMaravillaApplication.class, args);
+	}
+
+}

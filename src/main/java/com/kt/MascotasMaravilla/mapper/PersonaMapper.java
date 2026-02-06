4f9abd22359a58a6c6f1f8405a9ca4b325ce2fd4
Kevin <kfornier@gmail.com>
2026-02-05 21:14:28 -0300

    mapper Persona

---
+++ src/main/java/com/kt/MascotasMaravilla/mapper/PersonaMapper.java
@@ -0,0 +1,20 @@
+package com.kt.MascotasMaravilla.mapper;
+
+import com.kt.MascotasMaravilla.dto.PersonaDTO;
+import com.kt.MascotasMaravilla.model.Persona;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PersonaMapper {
+
+    Persona toEntity(PersonaDTO personaDTO);
+
+    PersonaDTO toDto(Persona persona);
+
+    List<PersonaDTO> toList(List<Persona> personaList);
+
+    void updatePersona(PersonaDTO personaDTO, @MappingTarget Persona persona);
+}
